0657aa48080a1c620c12da5612b3711c49c1a206
tarunclub <tarunkm__@outlook.com>
2023-05-20 09:35:19 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+todos = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=['POST'])
+def add_todo():
+    todo = request.form.get('todo')
+    todos.append(todo)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
